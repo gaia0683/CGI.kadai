06a2cdb7eff4ab4123d894f4ff32610faaff7ef7
bomute kim <kimbomte1996@icloud.com>
2022-07-11 12:39:25 +0900

    課題提出

---
+++ goya_quality_bad_pg.rb
@@ -0,0 +1,13 @@
+require 'pg'
+
+connection = PG::connect(dbname:"goya")
+connection.internal_encoding = "UTF-8"
+begin
+  result = connection.exec("select * from crops join prices on crops.give_for = prices.give_for where crops.quality = false;")
+
+result.each do |record|
+  puts "ゴーヤの長さ：#{record["length"]} ゴーヤの重さ：#{record["weight"]} ゴーヤの品質：#{record["quality"]}売った相手：#{record["give_for"]} 売った金額：#{record["price"]} 収穫した日付：#{record["date"]}"
+end
+ensure
+  connection.finish
+end
---
+++ test.rb
@@ -0,0 +1,24 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+  })
+  ['INT','TERM'].each{|signal|
+    Signal.trap(signal){ server.shutdown }
+  }
+
+  server.mount('/test',WEBrick::HTTPServlet::ERBHandler,'test.html.erb')
+
+  server.mount('/goya_not_myself',WEBrick::HTTPServlet::ERBHandler,'goya.html.erb')
+
+  server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+
+  server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+  server.mount('/goya_not_use_myself.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_not_use_myself.rb')
+
+  server.mount('/goya_quality_is_bad.cgi',WEBrick::HTTPServlet::CGIHandler, 'goya_quality_is_bad.rb')
+
+  server.start
